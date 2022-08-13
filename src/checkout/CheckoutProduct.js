e97f36b6702a3043fc0c28e6db82b51306b533a1
Shafia Ansar <shafiaansar142@gmail.com>
2022-08-13 11:01:14 -0800

    'Done with CheckoutProduct'

--- src/checkout/CheckoutProduct.js
+++ src/checkout/CheckoutProduct.js
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useStateValue } from '../StateProvider'
 import './CheckoutProduct.css'
 function CheckoutProduct({id, image , title,price , rating}) {
+    const [{basket} ,dispatch] = useStateValue()
+    const removeFromBasket = () => {
+    dispatch({
+        type:'REMOVE_FROM_BASKET',
+        id:id,
+        
+})
+    }
   return (
     <div className='checkoutProduct'>
       <img className='checkoutProduct_image' src={image} alt=''/>
@@ -16,7 +25,7 @@ function CheckoutProduct({id, image , title,price , rating}) {
             <p>⭐</p>
         ))
         }</div>
-        <button>Remove from Basket</button>
+        <button onClick={removeFromBasket}>Remove from Basket</button>
       </div>
     </div>
   )
